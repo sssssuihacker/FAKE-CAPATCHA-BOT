1cfcb4a126f38ce363c146f1b6d959a9ee57d138
sssssuihacker <mohamedhmdh9@gmail.com>
2024-05-30 16:25:47 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    'token': '', // Your bot token, activate all bot intents in discord developer portal.
+    'clientid': '', // Your discord bot id.
+    'guildid': '', // Id of a private server to load the bot's slash commands.
+    'ownerid': [''], // Add discord ids to the array to access the commands.
+    'webhook': '' // Your discord webhook url.
+}
